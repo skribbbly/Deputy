15eb458467153513a165a793dbec996de5f86872
Skribbbly-dev <skribbbly.dev@gmail.com>
2025-08-20 11:37:20 -0500

    first commit, basic movement

---
+++ deputy_project/actors/player/cam.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+@export_category("Camera Control")
+@export_group("Mouse")
+@export var mouse_sensitivity := 0.1
+
+@export var min_arm_length := .7    # closest‑in distance
+@export var max_arm_length := 2.5    # farthest‑out distance
+@export var max_tilt_for_zoom := 60  # degrees of pitch that maps to max length
+
+
+@onready var player := get_parent()
+@onready var cam := $SpringArm3D/Camera3D
+@onready var spring_arm := $SpringArm3D
+
+var pos_dif : Vector3
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	
+	if event is InputEventMouseMotion:
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	
+	#if !player.target:
+	_update_arm_length()
+
+func _process(delta: float) -> void:
+	player.rotation.y = get_parent().rotation.y
+
+func _update_arm_length() -> void:
+	# |rotation.x| goes 0 → max_tilt_for_zoom  (in radians)  
+	var tilt = abs(rotation.x)
+	var t := inverse_lerp(deg_to_rad(89), 0.0, rotation.x)
+
+	spring_arm.spring_length = lerp(min_arm_length, max_arm_length, t)
---
+++ deputy_project/actors/player/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+
+@onready var speed : float = run 
+@export var run := 5.1
+
+@onready var accel : float = ground_accel
+@export var ground_accel := 10.1
+
+@onready var drag : float = friction
+@export var friction := 10.1
+
+
+var dir : Vector3
+var mo_vec : Vector3
+var grav : Vector3
+
+
+@onready var mesh := $base_character
+@onready var anim := $AnimationTree
+@onready var climbray: RayCast3D = $base_character/ClimbRay
+
+
+func _process(delta: float) -> void:
+	mesh.global_position = global_position
+	if dir != Vector3.ZERO:
+			mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(-dir.x, -dir.z), 10 * delta)
+	
+	if Input.is_action_pressed("esc"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta: float) -> void:
+	var h_rot = global_transform.basis.get_euler().y
+	var input_vec := Vector2.ZERO
+	input_vec.x = Input.get_action_strength("d") - Input.get_action_strength("a")
+	input_vec.y = Input.get_action_strength("s") - Input.get_action_strength("w")
+	dir = Vector3(input_vec.x,0, input_vec.y).rotated(Vector3.UP, h_rot).normalized()
+	
+	if input_vec != Vector2.ZERO:
+		mo_vec = mo_vec.move_toward(dir * speed, accel * delta)
+	else:
+		mo_vec = mo_vec.move_toward(Vector3.ZERO, accel * delta)
+	
+	
+	if !is_on_floor():
+		grav.y -= 9.8 * delta
+		
+		if climbray.is_colliding():
+			if Input.is_action_just_pressed("space"):
+				global_position = climbray.get_collision_point()
+		
+	else:
+		grav.y = 0
+		if Input.is_action_just_pressed("space"):
+			if climbray.is_colliding():
+				global_position = climbray.get_collision_point()
+			else:
+				grav.y += 4
+	
+	anim.set("parameters/RunBlend/blend_amount", clamp(mo_vec.length() / speed, 0.0, 1.0))
+	
+	velocity = mo_vec + grav
+	
+	move_and_slide()
